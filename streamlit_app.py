73e7568ee2a51aa1cb16b7a78f6fb689ec673040
riedle03 <riedel@e-mirim.hs.kr>
2025-08-19 02:19:27 +0000

    01

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 이대형의 첫번째 스트림릿 앱")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "안녕하세요. 저는 미림마이스터고 국어교사 이대형입니다. 저만의 스트림릿 앱을 처음 만들어요."
 )
